51f7f719f65a0f8c5c44d507933bd6b9c7045517
Bishawa <bishawasunny4347@gmail.com>
2023-07-23 13:28:30 +0530

    Changed the test2.py file

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 def add_two_num(a, b):
     return a + b
+
+def multiply(a, b):
+    return a * b
+
---
+++ test2.py
@@ -0,0 +1,2 @@
+## This is a simple file 
+## Trying to resolve the error that this file is not commited in the repository
